9923d79a17d4e2743b0e22811d291084225b2317
jacksonnorris <jacksonnorr@gmail.com>
2022-01-25 19:05:49 -0500

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your Code Here
+let userName = window.prompt('Please enter your name: ')
+let userScore = 0;
+
+for (let i = 0; i < questions.length; i++) {
+    let question = questions[i]
+    let answer = window.prompt(question.text)
+    let userAnswer = answer.toUpperCase()
+    if(userAnswer === question.correctAnswer) { userScore = userScore + 10 }
+    console.log("Answer: " + userAnswer + ", correct: " + question.correctAnswer)
+}
+
+window.alert ('Your score is: ' + userScore)
